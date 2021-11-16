1434288e7eb827addb628b79a5e12121f1543f42
KKrusti <carlesming@hotmail.com>
2021-11-16 19:18:43 +0100

    refactoring some code

--- src/main/java/com/customer/controller/UserController.java
+++ src/main/java/com/customer/controller/UserController.java
@@ -5,14 +5,14 @@ import com.customer.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(value = "/v1/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
